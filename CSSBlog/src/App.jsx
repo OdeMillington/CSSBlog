ce4f00ecc76a7f86bedef75541446fba08e0a68c
Ode Millington <odemillington05@gmail.com>
2025-02-07 15:51:42 -0400

    Created changes

--- CSSBlog/src/App.jsx
+++ CSSBlog/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import HomePage from "/pages/HomePage"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <HomePage/>
     </>
   )
 }
---
+++ CSSBlog/src/pages/ProfilePage.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+
+// function HomePage() {return {h1 this is the home page}}  -- Alternative
+const ProfilePage = () => {
+    <h1>This is the Home Page</h1>
+}
+
+export default ProfilePage
