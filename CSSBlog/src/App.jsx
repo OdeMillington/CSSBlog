d1b896ad760bb4d052640347b767cb0ef2367fcf
Ode Millington <odemillington05@gmail.com>
2025-02-07 16:00:08 -0400

    Changes

--- CSSBlog/src/App.jsx
+++ CSSBlog/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import HomePage from "/pages/HomePage"
+import HomePage from "./pages/HomePage"
+import ProfilePage from "./pages/ProfilePage"
+import NewPostPage from "./pages/NewPostPage"
+
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 import './App.css'
 
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <HomePage/>
-    </>
+      <BrowserRouter>
+      
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/profile" element={<ProfilePage/>} />
+          <Route path="/post/new" element={<NewPostPage/>} />
+        </Routes>
+
+      </BrowserRouter>
   )
 }
 
